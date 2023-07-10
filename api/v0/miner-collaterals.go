f68e0c0e839458f11dc44612f3b549fafd5a200f
Jonathan <jpschwartz2@uwalumni.com>
2023-07-10 16:13:51 -0700

    Deprecate miner-collaterals endpoint

--- api/v0/miner-collaterals.go
+++ api/v0/miner-collaterals.go
@@ -1,41 +1,20 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-
-	"github.com/glifio/pools-metrics/common"
-	m "github.com/glifio/pools-metrics/metrics"
 )
 
 type MinerCollateralsHandlerRes struct {
 	TotalAgentCount       uint64 `json:"totalAgentCount"`
 	TotalMinerCollaterals string `json:"totalMinerCollaterals"`
 	TotalMinersCount      uint64 `json:"totalMinersCount"`
+	Error                 string `json:"error"`
 }
 
 func MinerCollaterals(w http.ResponseWriter, r *http.Request) {
-	sdk, err := common.NewSDK(r)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error initializing PoolsSDK: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	agentCount, minerCount, minerCollaterals, err := m.MinerCollaterals(r.Context(), sdk)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting metrics: %v", err), http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(&MinerCollateralsHandlerRes{
-		TotalAgentCount:       agentCount.Uint64(),
-		TotalMinerCollaterals: minerCollaterals.String(),
-		TotalMinersCount:      minerCount.Uint64(),
-	}); err != nil {
-		http.Error(w, fmt.Sprintf("Error encoding metrics to JSON: %v", err), http.StatusInternalServerError)
-		return
-	}
+	http.Error(w, fmt.Sprint("Endpoint deprecated - please use /miner-info instead"), http.StatusBadRequest)
+	return
 }
