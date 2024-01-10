9f22dd71c2e3cf868e5f207c4fdd637c792d4bb5
Jonathan <jpschwartz2@uwalumni.com>
2024-01-09 19:53:11 -0800

    Temp patch for APY

--- api/v0/apy.go
+++ api/v0/apy.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
-
-	"github.com/glifio/pools-metrics/common"
-	m "github.com/glifio/pools-metrics/metrics"
 )
 
 type ApyRes struct {
@@ -15,30 +12,10 @@ type ApyRes struct {
 }
 
 func Apy(w http.ResponseWriter, r *http.Request) {
-	sdk, err := common.NewSDK(r)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error initializing PoolsSDK: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	blockNumber, err := common.GetBlockNumberQP(r)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting block number: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	apy, err := m.Apy(r.Context(), sdk, blockNumber)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting apy: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	apy.Mul(apy, big.NewFloat(100))
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(&ApyRes{
-		Apy: apy,
+		Apy: big.NewFloat(0),
 	}); err != nil {
 		http.Error(w, fmt.Sprintf("Error encoding apy to JSON: %v", err), http.StatusInternalServerError)
 		return
